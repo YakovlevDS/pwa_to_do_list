2068792d540e9b2795aae706f5084e7007e387b4
Dmitriy Yakovlev <58001401+YakovlevDS@users.noreply.github.com>
2021-07-18 11:24:47 +0300

    fix err

--- js/app.js
+++
@@ -1,5 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register("pwabuilder-sw/pwabuilder-sw.js")
-    .then(()=>console.log("Зарегистрировали"))
-    .catch(()=>console.log("Получилась ошибка"));
-}
